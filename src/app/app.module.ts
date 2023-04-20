3bb302f9e567cb1935f8767f8639061783273cd4
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-20 22:24:05 +0200

    ajout du composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductComponent} from "./component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ProductComponent]
 })
 export class AppModule { }
