4d8fba24742743607c63c80735786f6b574f0dd1
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-20 22:06:11 +0200

    run build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-debugger
+//debugger
 @Component({
-  selector: 'approot',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css' ]
 })
 export class AppComponent {
   title = 'miniProject';
