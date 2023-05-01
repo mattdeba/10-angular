e7e9dd11eff182dd3e39bfe4d30888abc867b9d9
matthieu debarge <debarge.matthieu@gmail.com>
2023-05-01 18:25:49 +0200

    10.18

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,30 +1,33 @@
-import { Component } from "@angular/core";
+import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
 @Component({
   selector: "app",
   templateUrl: "template.html"
 })
 export class ProductComponent {
   model: Model = new Model();
-  getClasses(key: number): string {
-    let product = this.model.getProduct(key);
+  constructor(ref: ApplicationRef) {
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
+  }
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
+  }
+  getClassesByPosition(position: number): string {
+    let product = this.getProductByPosition(position);
     return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
   }
-  // getClassMap(key: number): Object {
+  // getClasses(key: number): string {
+  //     let product = this.model.getProduct(key);
+  //     return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
+  // }
+  // getStyles(key: number) {
   //     let product = this.model.getProduct(key);
   //     return {
-  //         "text-center bg-danger": product?.name == "Kayak",
-  //         "bg-info": (product?.price ?? 0) < 50
+  //         fontSize: "30px",
+  //         "margin.px": 100,
+  //         color: (product?.price?? 0) > 50 ? "red" : "green"
   //     };
   // }
-  // fontSizeWithUnits: string = "30px";
-  // fontSizeWithoutUnits: string= "30";
-  getStyles(key: number) {
-    let product = this.model.getProduct(key);
-    return {
-      fontSize: "30px",
-      "margin.px": 100,
-      color: (product?.price?? 0) > 50 ? "red" : "green"
-    };
-  }
 }
