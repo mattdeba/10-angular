31c856b6e8ca6b1e31083db45fd78dfa68cc4742
matthieu debarge <debarge.matthieu@gmail.com>
2023-05-02 19:07:39 +0200

    11.1

--- src/app/component.ts
+++ src/app/component.ts
@@ -14,20 +14,18 @@ export class ProductComponent {
   getProductByPosition(position: number): Product {
     return this.model.getProducts()[position];
   }
-  getClassesByPosition(position: number): string {
-    let product = this.getProductByPosition(position);
-    return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
-  }
-  // getClasses(key: number): string {
-  //     let product = this.model.getProduct(key);
+  // getClassesByPosition(position: number): string {
+  //     let product = this.getProductByPosition(position);
   //     return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
   // }
-  // getStyles(key: number) {
-  //     let product = this.model.getProduct(key);
-  //     return {
-  //         fontSize: "30px",
-  //         "margin.px": 100,
-  //         color: (product?.price?? 0) > 50 ? "red" : "green"
-  //     };
-  // }
+  getProduct(key: number): Product | undefined {
+    return this.model.getProduct(key);
+  }
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+  getProductCount(): number {
+    return this.getProducts().length;
+  }
+  targetName: string = "Kayak";
 }
