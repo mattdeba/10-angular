328d75612b95eed55eb3ce422df178941e832f88
Matthieu-D DEBARGE <Matthieu-D@fncuma.local>
2023-05-04 10:14:37 +0200

    12.3

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,4 +1,4 @@
-import { ApplicationRef, Component } from "@angular/core";
+import { Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
 @Component({
@@ -7,36 +7,11 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
   model: Model = new Model();
-  constructor(ref: ApplicationRef) {
-    (<any>window).appRef = ref;
-    (<any>window).model = this.model;
-  }
-  getProductByPosition(position: number): Product {
-    return this.model.getProducts()[position];
-  }
-  // getClassesByPosition(position: number): string {
-  //     let product = this.getProductByPosition(position);
-  //     return "p-2 " + ((product?.price ?? 0) < 50 ? "bg-info" : "bg-warning");
-  // }
   getProduct(key: number): Product | undefined {
     return this.model.getProduct(key);
   }
   getProducts(): Product[] {
     return this.model.getProducts();
   }
-  getProductCount(): number {
-    return this.getProducts().length;
-  }
-  targetName: string = "Kayak";
-  getKey(index: number, product: Product) {
-    return product.id;
-  }
-  counter: number = 1;
-
-  get nextProduct(): Product | undefined {
-    return this.model.getProducts().shift();
-  }
-  getProductPrice(index: number): number {
-    return Math.floor(this.getProduct(index)?.price ?? 0);
-  }
+  selectedProduct: string | undefined;
 }
