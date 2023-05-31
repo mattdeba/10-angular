8f333f8c7cce430be2bc1a46e7f6959ae058f37f
Matthieu-D DEBARGE <Matthieu-D@fncuma.local>
2023-05-31 09:54:23 +0200

    Listing 13-3 Le contenu du fichier attr.directive.ts dans le dossier src/app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ProductComponent} from "./component";
 import { FormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
 
 
 @NgModule({
   declarations: [
-    AppComponent, ProductComponent
+    ProductComponent, PaAttrDirective
   ],
   imports: [
     BrowserModule, FormsModule,
