5a82a60fe45db8f564f9e23fd21f38275d244fc9
Matthieu-D <matthieu.debarge@cuma.fr>
2024-08-23 18:12:21 +0200

    Liste 13-7Lecture d'un attribut dans le fichier attr.directive.ts dans le dossier src/app

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Attribute, Directive, ElementRef } from "@angular/core";
 @Directive({
   selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-  constructor(element: ElementRef) {
-    element.nativeElement.classList.add("bg-success", "fw-bold");
+  constructor(element: ElementRef, @Attribute("pa-attr-class") bgClass: string) {
+    element.nativeElement.classList.add(bgClass || "table-success", "fw-bold");
   }
 }
